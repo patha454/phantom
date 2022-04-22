b2d8f72e744c9fc1d98e83798ab51377aaac0450
H Paterson <harley.paterson@tracplus.com>
2022-04-22 20:53:03 +1200

    Feature: Write on boot.

--- library.c
+++ library.c
@@ -1,8 +1,10 @@
 #include "library.h"
 
 extern void sys_exit_group(int status);
+extern long sys_write(int fd, void* buf, unsigned long count);
 
 void start(void)
 {
+    sys_write(1, "Test", 4);
     sys_exit_group(69);
 }
