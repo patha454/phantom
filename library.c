7dc318ff955020293df2388ec38a755d9e9d2dcb
H Paterson <harley.paterson@tracplus.com>
2022-04-24 22:20:30 +1200

    Feature: Link freestanding `printf` library.

--- library.c
+++ library.c
@@ -1,10 +1,17 @@
 #include "library.h"
+#include "printf/printf.h"
 
 extern void sys_exit_group(int status);
 extern long sys_write(int fd, void* buf, unsigned long count);
 
+extern void putchar_(char c)
+{
+    sys_write(1, &c, 1);
+}
+
 void start(void)
 {
+    printf_("Hello, world!");
     sys_write(1, "Test", 4);
     sys_exit_group(69);
 }
