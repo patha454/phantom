c2d7ea440c19b62ee5450baf5381ba66b5ab454f
H Paterson <harley.paterson@tracplus.com>
2022-04-24 23:21:52 +1200

    Feature: Use `noreturn` annotations.

--- library.c
+++ library.c
@@ -1,7 +1,8 @@
 #include "library.h"
 #include "printf/printf.h"
+#include <stdnoreturn.h>
 
-extern void sys_exit_group(int status);
+extern noreturn void sys_exit_group(int status);
 extern long sys_write(int fd, void* buf, unsigned long count);
 
 extern void putchar_(char c)
@@ -9,9 +10,9 @@ extern void putchar_(char c)
     sys_write(1, &c, 1);
 }
 
-void start(void)
+noreturn void start(void)
 {
-    printf_("Hello, world!");
+    printf_("Hello, world! %p", &start);
     sys_write(1, "Test", 4);
     sys_exit_group(69);
 }
