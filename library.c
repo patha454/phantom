698b40298d6a067d4524b8c7dbc026c0078392de
H Paterson <26947190+patha454@users.noreply.github.com>
2022-05-23 22:41:45 +1200

    Bugfix: Use passed, correct stack pointer.

--- library.c
+++ library.c
@@ -13,8 +13,8 @@ noreturn void main(void* stackPointer)
 {
     long* argc = (long*) stackPointer;
     printf_("sp: %p\n", argc);
-    printf_("&argc: %p\n", argc + 6);
-    printf_("Argc: %d\n", *(argc + 6));
+    printf_("&argc: %p\n", argc + 0);
+    printf_("Argc: %d\n", *(argc + 0));
     sys_write(1, "Test\n", 5);
     sys_exit_group(69);
 }
